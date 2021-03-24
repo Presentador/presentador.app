2cd7347527de3623ea73d605b36b90f41ea76278
kbariotis <konmpar@gmail.com>
2021-03-25 00:03:46 +0200

    disable eslint line

--- src/context.ts
+++ src/context.ts
@@ -24,7 +24,7 @@ export function useSlideState() {
       }
     }
     update();
-  }, [currentSlide, elements]);
+  }, [currentSlide, elements]); // eslint-disable-line
 
   function addSlide() {
     setSlideState([...slideState, "normal"]);
