ddf9a0231d76117ef03694ff49b4914661d1b538
kbariotis <konmpar@gmail.com>
2021-04-02 15:55:39 +0000

    Deploying to gh-pages from @ kbariotis/presentador.app@a06fe7d9c4a5b050f3a46774f7a47a04d09f28c3 🚀

--- static/js/11.9485ccaa.chunk.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see 11.9485ccaa.chunk.js.LICENSE.txt */
-(this.webpackJsonppresentit=this.webpackJsonppresentit||[]).push([[11],{41:function(e,t,n){"use strict";n.r(t);var r=n(5),c=n(6);t.default=function(){var e=Object(c.a)(r.mark((function e(t){var n,c,i=arguments;return r.wrap((function(e){for(;;)switch(e.prev=e.next){case 0:n=i.length>1&&void 0!==i[1]?i[1]:{},(c=document.createElement("a")).download=n.fileName||"Untitled",c.href=URL.createObjectURL(t),c.addEventListener("click",(function(){setTimeout((function(){return URL.revokeObjectURL(c.href)}),3e4)})),c.click();case 3:case"end":return e.stop()}}),e)})));return function(t){return e.apply(this,arguments)}}()}}]);
-//# sourceMappingURL=11.9485ccaa.chunk.js.map
