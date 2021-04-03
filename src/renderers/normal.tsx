aa34fbf31d17a458755f3c3ca9a59d58fe3ee365
kbariotis <konmpar@gmail.com>
2021-04-03 15:30:19 +0300

    add more formating conditions to normal state

--- src/renderers/normal.tsx
+++ src/renderers/normal.tsx
@@ -28,14 +28,61 @@ export function NormalRenderer({ children }: { children: JSX.Element[] }) {
 }
 
 export const NormalBuilder: Builder = {
-  add: (type, elements) => {
-    if (type === "heading" && !elements.length) return "singleHeader";
-    if (type === "image" && !elements.length) return "image";
-    if (type === "blockquote" && !elements.length) return "blockquote";
+  add: (type, existingElements) => {
+    if (type === "heading" && !existingElements.length) return "singleHeader";
+    if (type === "image" && !existingElements.length) return "image";
+    if (type === "blockquote" && !existingElements.length) return "blockquote";
 
     return "normal";
   },
-  remove: (type, elements) => {
+  remove: (type, remainingElements) => {
+    const remainingHeading = remainingElements.filter(
+      (item) => item.type === "heading"
+    ).length;
+    const remainingImage = remainingElements.filter(
+      (item) => item.type === "image"
+    ).length;
+    const remainingBlockquote = remainingElements.filter(
+      (item) => item.type === "blockquote"
+    ).length;
+    const remainingParagraph = remainingElements.filter(
+      (item) => item.type === "paragraph"
+    ).length;
+    const remainingList = remainingElements.filter(
+      (item) => item.type === "list"
+    ).length;
+
+    if (remainingElements.length === 2 && remainingHeading === 2) {
+      return "twoHeaders";
+    }
+    if (remainingElements.length === 1 && remainingBlockquote === 1) {
+      return "blockquote";
+    }
+    if (remainingElements.length === 1 && remainingHeading === 1) {
+      return "singleHeader";
+    }
+
+    if (remainingElements.length === 2 && remainingHeading === 1) {
+      if (remainingBlockquote === 1) {
+        return "headerBlockquote";
+      }
+      if (remainingImage === 1) {
+        return "headerImage";
+      }
+      if (remainingParagraph === 1) {
+        return "headerSingleParagraph";
+      }
+      if (remainingList === 1) {
+        return "headerList";
+      }
+    }
+
+    if (remainingElements.length > 2 && remainingHeading === 1) {
+      if (remainingParagraph > 1) {
+        return "headerManyParagraphs";
+      }
+    }
+
     return "normal";
   },
 };
