1671c0e92ffb82c362c3e185bad49edbd0855f14
kbariotis <konmpar@gmail.com>
2021-04-03 19:11:22 +0300

    style buttons

--- src/components/menu/StyledButton.tsx
+++ src/components/menu/StyledButton.tsx
@@ -4,6 +4,23 @@ const StyledButton = styled.button`
   padding: 1em;
   margin-right: 0.3em;
   font-size: 1em;
+  border: 1px solid #737373;
+  border-radius: 5px;
+  color: #737373;
+
+  &:hover,
+  &:active,
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    box-shadow: inset 0 0 1px #000000;
+  }
+
+  &:hover {
+    color: black;
+  }
 `;
 
 export default StyledButton;
