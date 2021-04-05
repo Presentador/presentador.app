db1bb720b95955bc3a060250c20cca685f6e1028
kbariotis <konmpar@gmail.com>
2021-04-05 18:30:02 +0300

    shorten elements island

--- src/components/Elements.tsx
+++ src/components/Elements.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useEffect, useRef, useState, useContext } from "react";
 
 import { SlidesContext } from "../context/slides";
 import { DeckContext } from "../context/deck";
@@ -7,88 +7,163 @@ import StyledButton from "./menu/StyledButton";
 import Image from "./menu/Image";
 
 function Elements() {
+  const ref = useRef<HTMLDivElement | null>(null);
   const { addElement } = useContext(SlidesContext);
   const { currentSlide } = useContext(DeckContext);
 
+  const [headingSelected, setHeadingSelected] = useState(false);
+  const [listSelected, setListSelected] = useState(false);
+
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setHeadingSelected(false);
+        setListSelected(false);
+      }
+    }
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  if (headingSelected) {
+    return (
+      <div ref={ref}>
+        <StyledButton
+          title="Heading 1"
+          onClick={() => {
+            setHeadingSelected(false);
+            addElement(currentSlide, {
+              id: new Date().getTime(),
+              type: "heading",
+              level: 1,
+              value: "Heading 1",
+            });
+          }}
+        >
+          H1
+        </StyledButton>
+        <StyledButton
+          title="Heading 2"
+          onClick={() => {
+            setHeadingSelected(false);
+            addElement(currentSlide, {
+              id: new Date().getTime(),
+              type: "heading",
+              level: 2,
+              value: "Heading 2",
+            });
+          }}
+        >
+          H2
+        </StyledButton>
+        <StyledButton
+          title="Heading 3"
+          onClick={() => {
+            setHeadingSelected(false);
+            addElement(currentSlide, {
+              id: new Date().getTime(),
+              type: "heading",
+              level: 3,
+              value: "Heading 3",
+            });
+          }}
+        >
+          H3
+        </StyledButton>
+        <StyledButton
+          title="Heading 4"
+          onClick={() => {
+            setHeadingSelected(false);
+            addElement(currentSlide, {
+              id: new Date().getTime(),
+              type: "heading",
+              level: 4,
+              value: "Heading 4",
+            });
+          }}
+        >
+          H4
+        </StyledButton>
+        <StyledButton
+          title="Heading 5"
+          onClick={() => {
+            setHeadingSelected(false);
+            addElement(currentSlide, {
+              id: new Date().getTime(),
+              type: "heading",
+              level: 5,
+              value: "Heading 5",
+            });
+          }}
+        >
+          H5
+        </StyledButton>
+        <StyledButton
+          title="Heading 6"
+          onClick={() => {
+            setHeadingSelected(false);
+            addElement(currentSlide, {
+              id: new Date().getTime(),
+              type: "heading",
+              level: 6,
+              value: "Heading 6",
+            });
+          }}
+        >
+          H6
+        </StyledButton>
+      </div>
+    );
+  }
+
+  if (listSelected) {
+    return (
+      <div ref={ref}>
+        <StyledButton
+          title="List"
+          onClick={() => {
+            setListSelected(false);
+            addElement(currentSlide, {
+              id: new Date().getTime(),
+              type: "list",
+              value: "<li>Point one to make</li>",
+              listType: "unordered",
+            });
+          }}
+        >
+          Ul
+        </StyledButton>
+        <StyledButton
+          title="List"
+          onClick={() => {
+            setListSelected(false);
+            addElement(currentSlide, {
+              id: new Date().getTime(),
+              type: "list",
+              value: "<li>Point one to make</li>",
+              listType: "ordered",
+            });
+          }}
+        >
+          Ol
+        </StyledButton>
+      </div>
+    );
+  }
+
   return (
-    <>
+    <div ref={ref}>
       <StyledButton
-        title="Heading 1"
-        onClick={() =>
-          addElement(currentSlide, {
-            id: new Date().getTime(),
-            type: "heading",
-            level: 1,
-            value: "Heading 1",
-          })
-        }
+        title="Heading"
+        onClick={(event) => {
+          event.stopPropagation();
+          setHeadingSelected(true);
+        }}
       >
-        H1
-      </StyledButton>
-      <StyledButton
-        title="Heading 2"
-        onClick={() =>
-          addElement(currentSlide, {
-            id: new Date().getTime(),
-            type: "heading",
-            level: 2,
-            value: "Heading 2",
-          })
-        }
-      >
-        H2
-      </StyledButton>
-      <StyledButton
-        title="Heading 3"
-        onClick={() =>
-          addElement(currentSlide, {
-            id: new Date().getTime(),
-            type: "heading",
-            level: 3,
-            value: "Heading 3",
-          })
-        }
-      >
-        H3
-      </StyledButton>
-      <StyledButton
-        title="Heading 4"
-        onClick={() =>
-          addElement(currentSlide, {
-            id: new Date().getTime(),
-            type: "heading",
-            level: 4,
-            value: "Heading 4",
-          })
-        }
-      >
-        H4
-      </StyledButton>
-      <StyledButton
-        title="Heading 5"
-        onClick={() =>
-          addElement(currentSlide, {
-            id: new Date().getTime(),
-            type: "heading",
-            level: 5,
-            value: "Heading 5",
-          })
-        }
-      >
-        H5
-      </StyledButton>
-      <StyledButton
-        title="Heading 6"
-        onClick={() =>
-          addElement(currentSlide, {
-            id: new Date().getTime(),
-            type: "heading",
-            level: 6,
-            value: "Heading 6",
-          })
-        }
-      >
-        H6
+        H
       </StyledButton>
       <StyledButton
         title="Paragraph"
@@ -106,29 +181,12 @@ function Elements() {
       <Image />
       <StyledButton
         title="List"
-        onClick={() =>
-          addElement(currentSlide, {
-            id: new Date().getTime(),
-            type: "list",
-            value: "<li>Point one to make</li>",
-            listType: "unordered",
-          })
-        }
+        onClick={(event) => {
+          event.stopPropagation();
+          setListSelected(true);
+        }}
       >
-        Ul
-      </StyledButton>
-      <StyledButton
-        title="List"
-        onClick={() =>
-          addElement(currentSlide, {
-            id: new Date().getTime(),
-            type: "list",
-            value: "<li>Point one to make</li>",
-            listType: "ordered",
-          })
-        }
-      >
-        Ol
+        Li
       </StyledButton>
       <StyledButton
         title="Quote"
@@ -154,7 +212,7 @@ function Elements() {
       >
         F
       </StyledButton>
-    </>
+    </div>
   );
 }
 
