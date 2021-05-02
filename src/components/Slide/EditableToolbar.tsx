19e6ea8c541334ec236754b89bcc0456345f2a38
kbariotis <konmpar@gmail.com>
2021-05-02 21:24:04 +0300

    initial commit witj link support

--- src/components/Slide/EditableToolbar.tsx
+++ src/components/Slide/EditableToolbar.tsx
@@ -1,10 +1,13 @@
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import styled from "styled-components";
 import { Popover } from "react-text-selection-popover";
 import { position } from "caret-pos";
 import { ReactComponent as BoldIcon } from "bootstrap-icons/icons/type-bold.svg";
 import { ReactComponent as ItalicIcon } from "bootstrap-icons/icons/type-italic.svg";
 import { ReactComponent as ClearFormattingIcon } from "bootstrap-icons/icons/x.svg";
+import { ReactComponent as LinkIcon } from "bootstrap-icons/icons/link-45deg.svg";
+import { ReactComponent as CheckIcon } from "bootstrap-icons/icons/check.svg";
+import { ReactComponent as CancelIcon } from "bootstrap-icons/icons/x.svg";
 
 const StyledPopover = styled.div<{ left: number; top: number }>`
   position: absolute;
@@ -23,7 +26,75 @@ const ToolButton = styled.button`
   margin-right: 0.2em;
 `;
 
+/**
+ * INCOMPLETE
+ * https://stackoverflow.com/questions/13949059/persisting-the-changes-of-range-objects-after-selection-in-html/13950376#13950376
+ */
+const saveSelection = function (containerEl: any) {
+  const selection = window.getSelection();
+  if (selection) {
+    const range = selection.getRangeAt(0);
+    var preSelectionRange = range.cloneRange();
+    preSelectionRange.selectNodeContents(containerEl);
+    preSelectionRange.setEnd(range.startContainer, range.startOffset);
+    var start = preSelectionRange.toString().length;
+
+    return {
+      start: start,
+      end: start + range.toString().length,
+    };
+  }
+};
+
+const restoreSelection = function (containerEl: any, savedSel: any) {
+  var charIndex = 0,
+    range = document.createRange();
+  range.setStart(containerEl, 0);
+  range.collapse(true);
+  var nodeStack = [containerEl],
+    node,
+    foundStart = false,
+    stop = false;
+
+  while (!stop && (node = nodeStack.pop())) {
+    if (node.nodeType == 3) {
+      var nextCharIndex = charIndex + node.length;
+      if (
+        !foundStart &&
+        savedSel.start >= charIndex &&
+        savedSel.start <= nextCharIndex
+      ) {
+        range.setStart(node, savedSel.start - charIndex);
+        foundStart = true;
+      }
+      if (
+        foundStart &&
+        savedSel.end >= charIndex &&
+        savedSel.end <= nextCharIndex
+      ) {
+        range.setEnd(node, savedSel.end - charIndex);
+        stop = true;
+      }
+      charIndex = nextCharIndex;
+    } else {
+      var i = node.childNodes.length;
+      while (i--) {
+        nodeStack.push(node.childNodes[i]);
+      }
+    }
+  }
+
+  var sel = window.getSelection();
+  if (sel) {
+    sel.removeAllRanges();
+    sel.addRange(range);
+  }
+};
+
 function EditableToolbar(_: any, ref: any) {
+  const [showLinkText, setShowLinkText] = useState(false);
+  const [linkText, setLinkText] = useState("");
+
   if (!ref.current) {
     return <></>;
   }
@@ -33,59 +104,116 @@ function EditableToolbar(_: any, ref: any) {
       render={({ clientRect, isCollapsed, textContent }) => {
         if (clientRect == null || isCollapsed) return null;
 
+        const range = saveSelection(ref.current);
+
         return (
           <StyledPopover
             left={clientRect.left + clientRect.width / 2}
             top={clientRect.top - 40}
           >
-            <ToolButton
-              onMouseDown={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-                if (ref.current) {
-                  const pos = position(ref.current);
-                  ref.current.innerHTML = ref.current.innerHTML.replace(
-                    `${textContent}`,
-                    `<b>${textContent}</b>`
-                  );
-                  position(ref.current, pos.pos);
-                }
-              }}
-            >
-              <BoldIcon />
-            </ToolButton>
-            <ToolButton
-              onMouseDown={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                if (ref.current) {
-                  const pos = position(ref.current);
-                  ref.current.innerHTML = ref.current.innerHTML.replace(
-                    `${textContent}`,
-                    `<i>${textContent}</i>`
-                  );
-                  position(ref.current, pos.pos);
-                }
-              }}
-            >
-              <ItalicIcon />
-            </ToolButton>
-            <ToolButton
-              onMouseDown={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                if (ref.current) {
-                  const pos = position(ref.current);
-                  ref.current.innerHTML = ref.current.innerHTML.replace(
-                    /<\/?[b|i]+(>|$)/g,
-                    ""
-                  );
-                  position(ref.current, pos.pos);
-                }
-              }}
-            >
-              <ClearFormattingIcon />
-            </ToolButton>
+            {showLinkText && (
+              <>
+                <input
+                  type="text"
+                  onMouseDown={(e: any) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    e.target.focus();
+                  }}
+                  onChange={(e: any) => setLinkText(e.target.value)}
+                />
+                <ToolButton
+                  onMouseDown={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    if (ref.current) {
+                      setShowLinkText(false);
+                      ref.current.innerHTML = ref.current.innerHTML.replace(
+                        `${textContent}`,
+                        `<b>${textContent}</b>`
+                      );
+                      if (range) {
+                        restoreSelection(ref.current, range);
+                      }
+                    }
+                  }}
+                >
+                  <CheckIcon />
+                </ToolButton>
+                <ToolButton
+                  onMouseDown={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    if (ref.current) {
+                      setShowLinkText(false);
+                    }
+                  }}
+                >
+                  <CancelIcon />
+                </ToolButton>
+              </>
+            )}
+            {!showLinkText && (
+              <>
+                <ToolButton
+                  onMouseDown={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    setShowLinkText(true);
+                  }}
+                >
+                  <LinkIcon />
+                </ToolButton>
+                <ToolButton
+                  onMouseDown={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    if (ref.current) {
+                      ref.current.innerHTML = ref.current.innerHTML.replace(
+                        `${textContent}`,
+                        `<b>${textContent}</b>`
+                      );
+                      if (range) {
+                        restoreSelection(ref.current, range);
+                      }
+                    }
+                  }}
+                >
+                  <BoldIcon />
+                </ToolButton>
+                <ToolButton
+                  onMouseDown={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if (ref.current) {
+                      ref.current.innerHTML = ref.current.innerHTML.replace(
+                        `${textContent}`,
+                        `<i>${textContent}</i>`
+                      );
+                      if (range) {
+                        restoreSelection(ref.current, range);
+                      }
+                    }
+                  }}
+                >
+                  <ItalicIcon />
+                </ToolButton>
+                <ToolButton
+                  onMouseDown={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    if (ref.current) {
+                      ref.current.innerHTML = ref.current.innerHTML.replace(
+                        /<\/?[b|i]+(>|$)/g,
+                        ""
+                      );
+                    }
+                  }}
+                >
+                  <ClearFormattingIcon />
+                </ToolButton>
+              </>
+            )}
           </StyledPopover>
         );
       }}
